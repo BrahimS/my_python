c5bfa78962695ab0487c4cd2fdfc6ad7ad9822c5
Brahim Sourny <brahimsourny@MacBook-Pro-de-Brahim.local>
2023-08-25 09:44:16 +0200

    add function courses

--- basics/function.py
+++ basics/function.py
@@ -1,3 +1,54 @@
+# Python Functions
+# A function is a block of code which only runs when it is called.
+# You can pass data, known as parameters, into a function.
+# A function can return data as a result.
+
+# Creating a Function
+# In Python a function is defined using the def keyword:
+def my_function():
+  print("Hello this is a function")
+
+# Calling a Function
+# To call a function, use the function name followed by parenthesis:
+my_function()
+
+# Arguments
+# Information can be passed into functions as arguments.
+
+# Arguments are specified after the function name, inside the parentheses.
+# You can add as many arguments as you want, just separate them with a comma.
+
+# The following example has a function with one argument (fname).
+# When the function is called, we pass along a first name, which is used inside the function to print the full name:
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil") # Emil Refsne
+my_function("Tobias") # Tobias Refsnes
+my_function("Linus") # Linus Refsnes
+
+# Parameters or Arguments?
+# The terms parameter and argument can be used for the same thing: information that are passed into a function.
+
+# From a function's perspective:
+
+# A parameter is the variable listed inside the parentheses in the function definition.
+
+# An argument is the value that is sent to the function when it is called.
+
+# Number of Arguments
+# By default, a function must be called with the correct number of arguments.
+# Meaning that if your function expects 2 arguments, you have to call the function with 2 arguments, not more, and not less.
+
+# This function expects 2 arguments, and gets 2 arguments:
+
+def my_function2(fname, lname):
+  print(fname + " " + lname)
+
+my_function2("Emil", "Baba") # Emil Baba
+
+
+
 
 # def greeting(time):
 #     if time <= 12:
